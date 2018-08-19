3c4bca0769039a827cd6a9d5f727e8c7d5a65b82
Rasleen Kaur <rasleen0209@gmail.com>
2018-08-20 02:51:07 +0530

    My first Django Blog Post

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post  #imports our post from models.py
+
+
+admin.site.register(Post)
+'''
+As you can see, we import (include) the Post model defined in the previous chapter.
+ To make our model visible on the admin page, we need to register the model with admin.site.register(Post).
+'''
