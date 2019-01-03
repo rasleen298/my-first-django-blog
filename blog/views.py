2ba2eaebd9b3ab0c29d6014c31b195b085f2394c
Rasleen Kaur <rasleen0209@gmail.com>
2019-01-03 21:35:02 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
+
 def post_list(request):
     return render(request,'blog/post_list.html',{})
-=======
->>>>>>> 3c4bca0769039a827cd6a9d5f727e8c7d5a65b82
