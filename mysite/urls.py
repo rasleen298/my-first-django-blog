4d3b317bd8655235c59d73b8b0a10826eaac0a8e
Rasleen Kaur <rasleen0209@gmail.com>
2019-01-03 21:32:41 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,30 +1,22 @@
 """mysite URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+The `urlpatterns` list routes URLs to views. 
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    1.  from my_app import views
+    2. To urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1.  from other_app.views import Home
+    2. To urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. The include() function: from django.urls import include, path
+    2.To urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
                
-=======
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
->>>>>>> 3c4bca0769039a827cd6a9d5f727e8c7d5a65b82
-]
